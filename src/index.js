09f5d3c0bef7f7de27f982d84f6d5910fa27a663
romanbelei <45937404+romanbelei@users.noreply.github.com>
2022-06-21 14:35:43 +0200

    add basename

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter>
+        <BrowserRouter basename="goit-react-hw-08-phonebook">
           <App />
           {/* // basename="goit-react-hw-05-movies-router6" */}
         </BrowserRouter>
